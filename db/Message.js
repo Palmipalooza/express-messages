f6d569fce147230b1f3900ebbcd09dc9e071692c
Palmi Moller <palmimoller@Palmis-MacBook-Pro.local>
2020-01-14 13:14:21 -0700

    work on db Schema, methods, model. basic outline for express methods

--- db/Message.js
+++ db/Message.js
@@ -1,7 +1,22 @@
 //Message Model
 const db = require('./config');
 
-const Message = null;
+let MessageSchema = new db.Schema({
+  id: Number,
+  name: String,
+  message: String
+})
 
+/*
+add these methods:
+  create a message /api/messages
+  retrieve a list of all messages (output should be an array of objects)/api/messages
+  update a specific message /api/messages/1
+  delete a specific message /api/messages/1
+  get one specific message /api/messages/1
+*/
+
+const Message = null; //when methods are added, replace this line with:
+//const Message = db.model('Message', MessageSchema)
 
 module.exports = Message;
--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express();
 const Message = require('./db/Message');
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   res.status(404).send('That route does not exist');
 });
+
+/*
+add these methods: (with HTTP GET, POST, PUT, etc)
+  create a message /api/messages
+  retrieve a list of all messages (output should be an array of objects)/api/messages
+  update a specific message /api/messages/1
+  delete a specific message /api/messages/1
+  get one specific message /api/messages/1
+*/
+
+
+app.get('/api/messages', (req, res) => {
+  //invoke function in db to get all messages
+  // getall()
+    // .then(results => res.send(results))
+})
 
 const port = 3000;
 
